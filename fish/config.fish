3d1af535d69643299e4eca6d9a7a571314f89b85
FlashOnFire <flashonfire.dev@gmail.com>
2022-12-17 01:04:41 +0100

    added fish conf

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias ls="exa -lh"
+    alias rls="/bin/ls"
+    alias cat="bat"
+    alias rcat="/bin/cat"
+
+    alias vol="pamixer --set-volume"
+    alias light="xbacklight -set"
+    alias bt="sudo systemctl start bluetooth"
+    alias rbt="sudo rfkill block bluetooth;sudo rfkill unblock bluetooth;sudo systemctl restart bluetooth.service"
+
+    alias n="nmcli"
+end
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/flashonfire/.local/share/omf/themes/neolambda/fish_prompt.fish
